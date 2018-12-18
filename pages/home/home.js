29b1543fbdb25d814bc82b2de8fa95ae741346bc
lidongliangliqiuhe <742112250@qq.com>
2018-12-18 19:52:26 +0800

    Initial commit

---
+++ pages/home/home.js
@@ -0,0 +1,79 @@
+// pages/home/home.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list:[
+      { id: 1, img_url:"http://192.168.43.93:3000/img/banner1.png"},
+      { id: 2, img_url:"http://192.168.43.93:3000/img/banner2.png"},
+      { id: 3, img_url:"http://192.168.43.93:3000/img/banner3.png"},
+      { id: 4, img_url:"http://192.168.43.93:3000/img/banner4.png"},
+      ],
+    jiugong:[
+      { id: 1, img_url:"http://192.168.43.93:3000/img/jiugongge1.jpg",msg:"全部商品"},
+      { id: 2, img_url:"http://192.168.43.93:3000/img/jiugongge2.jpg",msg:"福鼎白茶"},
+      { id: 3, img_url:"http://192.168.43.93:3000/img/jiugongge3.jpg",msg:"龙井"},
+      { id: 4, img_url:"http://192.168.43.93:3000/img/jiugongge4.jpg",msg:"花茶"},
+      { id: 5, img_url:"http://192.168.43.93:3000/img/jiugongge5.jpg",msg:"礼茶"},
+      { id: 6, img_url:"http://192.168.43.93:3000/img/jiugongge6.jpg",msg:"茶具"}
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
