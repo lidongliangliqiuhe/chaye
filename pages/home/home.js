92a91d01c1716e2c27c6c54b9a3877635e6a09b3
lidongliangliqiuhe <742112250@qq.com>
2018-12-19 20:04:15 +0800

    ldl
    add jiugongge tapevent
    add shoplist page

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,9 +1,16 @@
 // pages/home/home.js
 Page({
-
+  handler1:function(e){
+    var pid=e.target.dataset.pid
+    
+    wx.navigateTo({
+      url: "/pages/shoplist/shoplist?pid" + pid,
+    })
+  },
   /**
    * 页面的初始数据
    */
+
   data: {
     list:[
       { id: 1, img_url:"http://192.168.43.93:3000/img/lunbo_1.jpg"},
@@ -12,12 +19,20 @@ Page({
       { id: 4, img_url:"http://192.168.43.93:3000/img/lunbo_4.jpg"},
       ],
     jiugong:[
-      { id: 1, img_url:"http://192.168.43.93:3000/img/jiugongge1.jpg",msg:"全部商品"},
-      { id: 2, img_url:"http://192.168.43.93:3000/img/jiugongge2.jpg",msg:"福鼎白茶"},
-      { id: 3, img_url:"http://192.168.43.93:3000/img/jiugongge3.jpg",msg:"龙井"},
-      { id: 4, img_url:"http://192.168.43.93:3000/img/jiugongge4.jpg",msg:"花茶"},
-      { id: 5, img_url:"http://192.168.43.93:3000/img/jiugongge5.jpg",msg:"礼茶"},
-      { id: 6, img_url:"http://192.168.43.93:3000/img/jiugongge6.jpg",msg:"茶具"}
+      { pid: 1, img_url:"http://192.168.43.93:3000/img/jiugongge1.jpg",msg:"全部商品"},
+      { pid: 2, img_url:"http://192.168.43.93:3000/img/jiugongge2.jpg",msg:"福鼎白茶"},
+      { pid: 3, img_url:"http://192.168.43.93:3000/img/jiugongge3.jpg",msg:"龙井"},
+      { pid: 4, img_url:"http://192.168.43.93:3000/img/jiugongge4.jpg",msg:"花茶"},
+      { pid: 5, img_url:"http://192.168.43.93:3000/img/jiugongge5.jpg",msg:"礼茶"},
+      { pid: 6, img_url:"http://192.168.43.93:3000/img/jiugongge6.jpg",msg:"茶具"}
+    ],
+    tjlist:[
+      { id: 1, img_url: "http://192.168.43.93:3000/img/tjlist/tjlist1.jpg", msg:"醉品叶界 中国茶礼 热销爆款 正山小种 特级 200g 红茶",price:"488.00"},
+      { id: 2, img_url: "http://192.168.43.93:3000/img/tjlist/tjlist2.jpg", msg:"太姥瑞祥龙 福鼎白茶 2014年特级寿眉巧克力紧压茶 荼省心1441  240g",price:"288.00"},
+      { id: 3, img_url: "http://192.168.43.93:3000/img/tjlist/tjlist3.jpg", msg:"凤山2006年安溪铁观音 拾叁老茶礼盒 陈香型252g",price:"390.00"},
+      { id: 4, img_url: "http://192.168.43.93:3000/img/tjlist/tjlist4.jpg", msg:"1392 广东单丛 来自火山口的茶海洋传奇系列精选礼盒 160g",price:"518.00"},
+      { id: 5, img_url: "http://192.168.43.93:3000/img/tjlist/tjlist5.jpg", msg:"一匠一品 三大名茶组合 2018年新茶 匠心之作 3盒装组合茶 150g 礼盒",price:"299.00"},
+      { id: 6, img_url: "http://192.168.43.93:3000/img/tjlist/tjlist6.jpg", msg:"醉品朴茶 武夷岩茶 水仙 本朴DHP1680-250 125g*2盒 中火 特级",price:"342.00"}
     ]
   },
 
--- pages/shoplist/shoplist.js
+++ pages/shoplist/shoplist.js
@@ -5,7 +5,14 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    shoplist: [
+      { id: 1, img_url: "http://192.168.43.93:3000/img/tjlist/tjlist1.jpg", msg: "醉品叶界 中国茶礼 热销爆款 正山小种 特级 200g 红茶", price: "488.00" },
+      { id: 2, img_url: "http://192.168.43.93:3000/img/tjlist/tjlist2.jpg", msg: "太姥瑞祥龙 福鼎白茶 2014年特级寿眉巧克力紧压茶 荼省心1441  240g", price: "288.00" },
+      { id: 3, img_url: "http://192.168.43.93:3000/img/tjlist/tjlist3.jpg", msg: "凤山2006年安溪铁观音 拾叁老茶礼盒 陈香型252g", price: "390.00" },
+      { id: 4, img_url: "http://192.168.43.93:3000/img/tjlist/tjlist4.jpg", msg: "1392 广东单丛 来自火山口的茶海洋传奇系列精选礼盒 160g", price: "518.00" },
+      { id: 5, img_url: "http://192.168.43.93:3000/img/tjlist/tjlist5.jpg", msg: "一匠一品 三大名茶组合 2018年新茶 匠心之作 3盒装组合茶 150g 礼盒", price: "299.00" },
+      { id: 6, img_url: "http://192.168.43.93:3000/img/tjlist/tjlist6.jpg", msg: "醉品朴茶 武夷岩茶 水仙 本朴DHP1680-250 125g*2盒 中火 特级", price: "342.00" }
+    ]
   },
 
   /**
